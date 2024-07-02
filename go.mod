2229de8871bfb3ea4b12a79ba672942783a6a858
tk1351 <takakikitsui0111@gmail.com>
2024-07-02 20:10:23 +0900

    feat: サードパーティー製のルーター

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22.4
 require github.com/go-playground/validator v9.31.0+incompatible
 
 require (
+	github.com/go-chi/chi/v5 v5.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 	"sync"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/go-playground/validator"
 )
 
@@ -18,6 +19,23 @@ type Comment struct {
 }
 
 func main() {
+	yes := 0
+	no := 0
+
+	r := chi.NewRouter()
+	r.Post("/poll/{answer}", func(w http.ResponseWriter, r *http.Request) {
+		if chi.URLParam(r, "answer") == "y" {
+			yes++
+		} else {
+			no++
+		}
+	})
+	r.Get("/result", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "賛成: %d, 反対: %d", yes, no)
+	})
+	r.Handle("/asset/*", http.StripPrefix("/asset/", http.FileServer(http.Dir("."))))
+	log.Fatal(http.ListenAndServe(":8080", r))
+
 	var mutex = &sync.RWMutex{}
 	comments := make([]Comment, 0, 100)
 
